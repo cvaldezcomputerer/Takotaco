f2a1da07cd83f8e1b39aa9705afb8f4716bcc291
Cristian Valdez <cvaldezana@gmail.com>
2025-02-28 21:01:53 +0900

    fixed 11ty

--- blog/.eleventy.js
+++ blog/.eleventy.js
@@ -1,11 +1,11 @@
 const { DateTime } = require("luxon");
 
-module.exports = function(eleventyConfig) {
+module.exports = function (eleventyConfig) {
   // Passthrough file copy
-   eleventyConfig.addPassthroughCopy("../www/css");
-  eleventyConfig.addPassthroughCopy("../www/js");
-  eleventyConfig.addPassthroughCopy("../www/images");
-  eleventyConfig.addPassthroughCopy("../www/audio");
+  eleventyConfig.addPassthroughCopy("./www/css");
+  eleventyConfig.addPassthroughCopy("./www/js");
+  eleventyConfig.addPassthroughCopy("./www/images");
+  eleventyConfig.addPassthroughCopy("./www/audio");
 
   // Add a filter to format dates
   eleventyConfig.addFilter("dateDisplay", (dateObj) => {
@@ -13,7 +13,7 @@ module.exports = function(eleventyConfig) {
   });
 
   // Create a collection for blog posts
-  eleventyConfig.addCollection("posts", function(collectionApi) {
+  eleventyConfig.addCollection("posts", function (collectionApi) {
     return collectionApi.getFilteredByGlob("src/posts/*.md");
   });
 
@@ -21,8 +21,10 @@ module.exports = function(eleventyConfig) {
     dir: {
       input: "src",
       output: "../www/blog", // Output to the blog directory in the existing website
-      includes: "src/_includes"
-      layouts: "_includes/layouts"
-    }
+      includes: "src/_includes",
+      layouts: "_includes/layouts",
+    },
+    htmlTemplateEngine: "njk",
+    markdownTemplateEngine: "njk",
   };
 };
--- blog/src/posts/first-post.md
+++ blog/src/posts/first-post.md
@@ -1,8 +1,10 @@
 ---
 title: My First Blog Post
 date: 2024-02-28
+layout: base.njk
+permalink: "posts/first-post/index.html"
 ---
 
 # My First Blog Post
 
-This is the content of my first blog post. I'm excited to start blogging!
+This is the content of my first blog post.
